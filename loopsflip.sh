1796d1746099e03f85dad6f967ff696693732273
BashaVali <bashavali222@gmail.com>
2022-09-26 12:11:00 +0530

    i have added loopsflip.sh

---
+++ loopsflip.sh
@@ -0,0 +1,15 @@
+#flipcoin
+Basila=0
+Basha=0
+for((i=0;i<=10;i++))
+do
+coin=$((RANDOM%2))
+     echo $coin
+
+if [ $coin -eq 0 ]
+then
+      echo "its tail"
+else
+      echo "its head"
+fi
+done
